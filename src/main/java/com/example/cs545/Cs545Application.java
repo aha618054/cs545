fe0bc8e96315bac494b0b488e5c516e3596ae6e5
aha618054 <aha@miu.edu>
2024-09-30 13:48:50 -0500

    Initial commit

---
+++ src/main/java/com/example/cs545/Cs545Application.java
@@ -0,0 +1,13 @@
+package com.example.cs545;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs545Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs545Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cs545/Cs545ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs545;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs545ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
